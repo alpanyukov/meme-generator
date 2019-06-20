a30ac7c567f25fe5729dcd296b22138520fbbb85
Alexander Panyukov <olenevodec@gmail.com>
2019-06-20 03:13:48 +0300

    убрал лишнее

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import React, { useState } from 'react';
-import { Text, Modal } from 'react-native';
-import { Screen } from './components/Screen';
 import { CreateMeme, MemeList } from './features/memes';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
@@ -25,28 +22,3 @@ const AppNavigator = createStackNavigator(
 );
 
 export const App = createAppContainer(AppNavigator);
-
-// export function App() {
-//   const [selectedMeme, selectMeme] = useState<MemeData | null>(null);
-
-//   return (
-//     <Screen>
-
-//       <MemeList onMemePress={selectMeme} />
-
-//       <Modal
-//         animationType="slide"
-//         transparent={false}
-//         visible={selectedMeme !== null}
-//         onRequestClose={() => {
-//           selectMeme(null);
-//         }}
-//         presentationStyle="fullScreen"
-//       >
-//         {selectedMeme && (
-//           <CreateMeme close={() => selectMeme(null)} id={selectedMeme.id} />
-//         )}
-//       </Modal>
-//     </Screen>
-//   );
-// }
